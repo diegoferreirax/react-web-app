813acd255705b838977f1a28f5b348640f39e407
Diego F Xavier <dbc.diego@unimedpoa.com.br>
2025-04-24 17:49:01 -0300

    implementacao inicial virtual scroll

--- src/modules/dashboard/index.tsx
+++ src/modules/dashboard/index.tsx
@@ -4,13 +4,130 @@ import { AppProvider, type Navigation } from '@toolpad/core/AppProvider';
 import { DashboardLayout } from '@toolpad/core/DashboardLayout';
 import { useDemoRouter } from '@toolpad/core/internal';
 import { useTranslation } from 'modules/translation/container';
+import { TableComponents, TableVirtuoso } from 'react-virtuoso';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import PeopleIcon from '@mui/icons-material/People';
 import InfoIcon from '@mui/icons-material/Info';
+import ListIcon from '@mui/icons-material/List';
 import AppTitle from './slots/app-title';
 import ToolbarActions from './slots/toolbar-actions';
 import SidebarFooter from './slots/sidebar-footer';
+
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import { TableContainer } from '@mui/material';
+
+
+interface Data {
+    id: number;
+    firstName: string;
+    lastName: string;
+    age: number;
+    phone: string;
+    state: string;
+}
+
+interface ColumnData {
+    dataKey: keyof Data;
+    label: string;
+    numeric?: boolean;
+    width?: number;
+}
+
+function createData(id: number): Data {
+    return {
+        id,
+        firstName: 'chance.first()',
+        lastName: 'chance.last()',
+        age: id,
+        phone: 'chance.phone()',
+        state: 'chance.state({ full: true })',
+    };
+}
+
+const columns: ColumnData[] = [
+    {
+        width: 100,
+        label: 'First Name',
+        dataKey: 'firstName',
+    },
+    {
+        width: 100,
+        label: 'Last Name',
+        dataKey: 'lastName',
+    },
+    {
+        width: 50,
+        label: 'Age',
+        dataKey: 'age',
+        numeric: true,
+    },
+    {
+        width: 110,
+        label: 'State',
+        dataKey: 'state',
+    },
+    {
+        width: 130,
+        label: 'Phone Number',
+        dataKey: 'phone',
+    },
+];
+
+const rows: Data[] = Array.from({ length: 200 }, (_, index) => createData(index));
+
+const VirtuosoTableComponents: TableComponents<Data> = {
+    Scroller: React.forwardRef<HTMLDivElement>((props, ref) => (
+        <TableContainer component={Paper} {...props} ref={ref} />
+    )),
+    Table: (props) => (
+        <Table {...props} sx={{ borderCollapse: 'separate', tableLayout: 'fixed' }} />
+    ),
+    TableHead: React.forwardRef<HTMLTableSectionElement>((props, ref) => (
+        <TableHead {...props} ref={ref} />
+    )),
+    TableRow,
+    TableBody: React.forwardRef<HTMLTableSectionElement>((props, ref) => (
+        <TableBody {...props} ref={ref} />
+    )),
+};
+
+function fixedHeaderContent() {
+    return (
+        <TableRow>
+            {columns.map((column) => (
+                <TableCell
+                    key={column.dataKey}
+                    variant="head"
+                    align={column.numeric || false ? 'right' : 'left'}
+                    style={{ width: column.width }}
+                    sx={{ backgroundColor: 'background.paper' }}
+                >
+                    {column.label}
+                </TableCell>
+            ))}
+        </TableRow>
+    );
+}
+
+function rowContent(_index: number, row: Data) {
+    return (
+        <React.Fragment>
+            {columns.map((column) => (
+                <TableCell
+                    key={column.dataKey}
+                    align={column.numeric || false ? 'right' : 'left'}
+                >
+                    {row[column.dataKey]}
+                </TableCell>
+            ))}
+        </React.Fragment>
+    );
+}
 
 const theme = createTheme({
     cssVariables: {
@@ -27,22 +144,6 @@ const theme = createTheme({
         },
     },
 });
-
-function PageContent({ pathname }: { pathname: string }) {
-    return (
-        <Box
-            sx={{
-                py: 4,
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                textAlign: 'center',
-            }}
-        >
-            <Typography>Dashboard content for {pathname}</Typography>
-        </Box>
-    );
-}
 
 interface DashboardProps {
     window?: () => Window;
@@ -66,6 +167,11 @@ const Dashboard: React.FC = (props: DashboardProps) => {
             icon: <PeopleIcon />,
         },
         {
+            segment: 'virtual-scroll',
+            title: translate('virtual_scroll'),
+            icon: <ListIcon />,
+        },
+        {
             segment: 'about',
             title: translate('about'),
             icon: <InfoIcon />,
@@ -74,6 +180,31 @@ const Dashboard: React.FC = (props: DashboardProps) => {
             kind: 'divider',
         },
     ];
+
+    const PageContent = ({ pathname }: { pathname: string }) => {
+        return (
+            <Box
+                sx={{
+                    py: 4,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    textAlign: 'center',
+                }}
+            >
+                {/* <Typography>Dashboard content for {pathname}</Typography> */}
+
+                <Paper style={{ height: 400, width: '100%' }}>
+                    <TableVirtuoso
+                        data={rows}
+                        components={VirtuosoTableComponents}
+                        fixedHeaderContent={fixedHeaderContent}
+                        itemContent={rowContent}
+                    />
+                </Paper>
+            </Box>
+        );
+    }
 
     return (
         <AppProvider
