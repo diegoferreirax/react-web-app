55a2b032849dfa6fccba74a2b3762e7ab214decb
Diego <diego.dev.gow@hotmail.com>
2025-05-11 04:28:48 -0300

    voltando ajuste react-router-dom

--- src/routes/protected-router/index.tsx
+++ src/routes/protected-router/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet } from 'react-router';
+import { Outlet } from 'react-router-dom';
 
 const ProtectedRoutes: React.FC = () => {
     // const isAuthenticated = true;
