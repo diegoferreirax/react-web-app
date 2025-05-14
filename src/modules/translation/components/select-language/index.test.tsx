0b605503b3aad622aa799baf9b3e936ef964d7f8
Diego <diego.dev.gow@hotmail.com>
2025-05-14 02:09:21 -0300

    configuração cobertura de testes com Jest

--- src/modules/translation/components/select-language/index.test.tsx
+++ src/modules/translation/components/select-language/index.test.tsx
@@ -3,132 +3,131 @@ import { TranslationProvider } from 'modules/translation/container';
 import SelectLanguage from 'modules/translation/components/select-language';
 import userEvent from '@testing-library/user-event';
 
-describe('Modules -> Translation', () => {
+describe(':: Modules', () => {
 
     beforeEach(() => {
         sessionStorage.clear();
     });
 
-    describe('Modules -> Translation -> Components -> SelectLanguage render tests', () => {
+    describe(':: Translation', () => {
 
-        test('Português selected language render test', () => {
-            sessionStorage.setItem('language', 'pt');
-    
-            render(
-                <TranslationProvider>
-                    <SelectLanguage />
-                </TranslationProvider>
-            );
-    
-            const imgElement = screen.getByAltText('Português');
-            expect(imgElement).toBeInTheDocument();
-    
-            const selectElement = screen.getByRole('combobox');
-            expect(selectElement).toBeInTheDocument();
-        });
-    
-        test('English selected language render test', () => {
-            sessionStorage.setItem('language', 'en');
-    
-            render(
-                <TranslationProvider>
-                    <SelectLanguage />
-                </TranslationProvider>
-            );
-    
-            const imgElement = screen.getByAltText('English');
-            expect(imgElement).toBeInTheDocument();
-    
-            const selectElement = screen.getByRole('combobox');
-            expect(selectElement).toBeInTheDocument();
-        });
-    
-        test('Spanish selected language render test', () => {
-            sessionStorage.setItem('language', 'es');
-    
-            render(
-                <TranslationProvider>
-                    <SelectLanguage />
-                </TranslationProvider>
-            );
-    
-            const imgElement = screen.getByAltText('Spanish');
-            expect(imgElement).toBeInTheDocument();
-    
-            const selectElement = screen.getByRole('combobox');
-            expect(selectElement).toBeInTheDocument();
-        });
-    });
+        describe('SelectLanguage component', () => {
+            it('should render the Portuguese flag when the selected language is Portuguese', () => {
+                sessionStorage.setItem('language', 'pt');
 
-    describe('Modules -> Translation -> Components -> SelectLanguage click render tests', () => {
+                render(
+                    <TranslationProvider>
+                        <SelectLanguage />
+                    </TranslationProvider>
+                );
 
-        test('SelectLanguage click when Português is on cache', () => {
-            sessionStorage.setItem('language', 'pt');
-    
-            render(
-                <TranslationProvider>
-                    <SelectLanguage />
-                </TranslationProvider>
-            );
-    
-            const selectElement = screen.getByRole('combobox');
-            expect(selectElement).toBeInTheDocument();
-    
-            userEvent.click(selectElement);
-    
-            const ptImg = screen.getAllByAltText('Português');
-            const enImg = screen.getByAltText('English');
-            const esImg = screen.getByAltText('Spanish');
-    
-            expect(ptImg).toHaveLength(2);
-            expect(enImg).toBeInTheDocument();
-            expect(esImg).toBeInTheDocument();
-        });
+                const imgElement = screen.getByAltText('Português');
+                expect(imgElement).toBeInTheDocument();
 
-        test('SelectLanguage click when English is on cache', () => {
-            sessionStorage.setItem('language', 'en');
-    
-            render(
-                <TranslationProvider>
-                    <SelectLanguage />
-                </TranslationProvider>
-            );
-    
-            const selectElement = screen.getByRole('combobox');
-            expect(selectElement).toBeInTheDocument();
-    
-            userEvent.click(selectElement);
-    
-            const ptImg = screen.getAllByAltText('English');
-            const enImg = screen.getByAltText('Português');
-            const esImg = screen.getByAltText('Spanish');
-    
-            expect(ptImg).toHaveLength(2);
-            expect(enImg).toBeInTheDocument();
-            expect(esImg).toBeInTheDocument();
-        });
+                const selectElement = screen.getByRole('combobox');
+                expect(selectElement).toBeInTheDocument();
+            });
 
-        test('SelectLanguage click when Spanish is on cache', () => {
-            sessionStorage.setItem('language', 'es');
-    
-            render(
-                <TranslationProvider>
-                    <SelectLanguage />
-                </TranslationProvider>
-            );
-    
-            const selectElement = screen.getByRole('combobox');
-            expect(selectElement).toBeInTheDocument();
-    
-            userEvent.click(selectElement);
-    
-            const ptImg = screen.getAllByAltText('Spanish');
-            const enImg = screen.getByAltText('Português');
-            const esImg = screen.getByAltText('English');
-    
-            expect(ptImg).toHaveLength(2);
-            expect(enImg).toBeInTheDocument();
-            expect(esImg).toBeInTheDocument();
+            it('should render the English flag when the selected language is English', () => {
+                sessionStorage.setItem('language', 'en');
+
+                render(
+                    <TranslationProvider>
+                        <SelectLanguage />
+                    </TranslationProvider>
+                );
+
+                const imgElement = screen.getByAltText('English');
+                expect(imgElement).toBeInTheDocument();
+
+                const selectElement = screen.getByRole('combobox');
+                expect(selectElement).toBeInTheDocument();
+            });
+
+            it('should render the Spanish flag when the selected language is Spanish', () => {
+                sessionStorage.setItem('language', 'es');
+
+                render(
+                    <TranslationProvider>
+                        <SelectLanguage />
+                    </TranslationProvider>
+                );
+
+                const imgElement = screen.getByAltText('Spanish');
+                expect(imgElement).toBeInTheDocument();
+
+                const selectElement = screen.getByRole('combobox');
+                expect(selectElement).toBeInTheDocument();
+            });
+
+            it('should display all language options when combobox is clicked and Portuguese is selected', () => {
+                sessionStorage.setItem('language', 'pt');
+
+                render(
+                    <TranslationProvider>
+                        <SelectLanguage />
+                    </TranslationProvider>
+                );
+
+                const selectElement = screen.getByRole('combobox');
+                expect(selectElement).toBeInTheDocument();
+
+                userEvent.click(selectElement);
+
+                const ptImg = screen.getAllByAltText('Português');
+                const enImg = screen.getByAltText('English');
+                const esImg = screen.getByAltText('Spanish');
+
+                expect(ptImg).toHaveLength(2);
+                expect(enImg).toBeInTheDocument();
+                expect(esImg).toBeInTheDocument();
+            });
+
+            it('should display all language options when combobox is clicked and English is selected', () => {
+                sessionStorage.setItem('language', 'en');
+
+                render(
+                    <TranslationProvider>
+                        <SelectLanguage />
+                    </TranslationProvider>
+                );
+
+                const selectElement = screen.getByRole('combobox');
+                expect(selectElement).toBeInTheDocument();
+
+                userEvent.click(selectElement);
+
+                const ptImg = screen.getAllByAltText('English');
+                const enImg = screen.getByAltText('Português');
+                const esImg = screen.getByAltText('Spanish');
+
+                expect(ptImg).toHaveLength(2);
+                expect(enImg).toBeInTheDocument();
+                expect(esImg).toBeInTheDocument();
+            });
+
+            it('should display all language options when combobox is clicked and Spanish is selected', () => {
+                sessionStorage.setItem('language', 'es');
+
+                render(
+                    <TranslationProvider>
+                        <SelectLanguage />
+                    </TranslationProvider>
+                );
+
+                const selectElement = screen.getByRole('combobox');
+                expect(selectElement).toBeInTheDocument();
+
+                userEvent.click(selectElement);
+
+                const ptImg = screen.getAllByAltText('Spanish');
+                const enImg = screen.getByAltText('Português');
+                const esImg = screen.getByAltText('English');
+
+                expect(ptImg).toHaveLength(2);
+                expect(enImg).toBeInTheDocument();
+                expect(esImg).toBeInTheDocument();
+            });
         });
 
     });
