ad65955181f611ce8c3b656bf3d8b1ed8921b4fb
Diego <diego.dev.gow@hotmail.com>
2025-05-15 21:27:35 -0300

    alteração branch para gh-pages

--- src/modules/client/list/ClientListPage.tsx
+++ src/modules/client/list/ClientListPage.tsx
@@ -33,7 +33,7 @@ const ClientListPage: React.FC = () => {
     const handleClientTableSelected = () => {
         if (clientTableSelected) {
             console.log(clientTableSelected);
-            console.log('log teste commit');
+            console.log('log teste commit 1');
         }
     };
 
